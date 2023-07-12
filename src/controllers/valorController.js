2316718efaf6f0ad97142f2960f7ab796c035800
Erison Cavalcante <erison7596e@gmail.com>
2023-07-12 17:29:12 -0300

    Atualizado a importação para o banco de dados

--- src/controllers/valorController.js
+++ src/controllers/valorController.js
@@ -13,44 +13,49 @@ async function importData(worksheet) {
 
   // Encontrar o índice das colunas desejadas
   const columnNames = {
-    DISTRIBUIDORA: -1,
+    'DISTRIBUIDORA': -1,
     'ID DISTRIBUIDORA': -1,
     'Nome para exibição': -1,
+    'ID LICENÇA': -1,
     'ID CUSTO LICENÇA': -1,
-    'Licenças (extraído do Office365)': -1,
     'Data/hora de criação': -1
   };
-  
+
   for (let col = range.s.c; col <= range.e.c; col++) {
-    const columnName = getCellValue(xlsx.utils.encode_col(col), range.s.r + 1, worksheet);
-    
+    const columnName = getCellValue(xlsx.utils.encode_col(col), range.s.r + 2, worksheet);
+    console.log('Nome da coluna:', columnName);
+
     if (columnNames.hasOwnProperty(columnName)) {
       columnNames[columnName] = col;
     }
   }
 
+  console.log('Colunas encontradas:', columnNames);
+
   // Verificar se todas as colunas foram encontradas
   const missingColumns = Object.keys(columnNames).filter((columnName) => columnNames[columnName] === -1);
-  
+
   if (missingColumns.length > 0) {
     throw new Error(`Colunas faltando: ${missingColumns.join(', ')}`);
   }
 
   // Importar os dados apenas das colunas desejadas
   for (let row = range.s.r + 2; row <= range.e.r; row++) {
-    const distribuidora = getCellValue(xlsx.utils.encode_col(columnNames.DISTRIBUIDORA), row, worksheet);
+    const distribuidora = getCellValue(xlsx.utils.encode_col(columnNames['DISTRIBUIDORA']), row, worksheet);
     const idDistribuidora = getCellValue(xlsx.utils.encode_col(columnNames['ID DISTRIBUIDORA']), row, worksheet);
     const nomeExibicao = getCellValue(xlsx.utils.encode_col(columnNames['Nome para exibição']), row, worksheet);
+    const idLicenca = getCellValue(xlsx.utils.encode_col(columnNames['ID LICENÇA']), row, worksheet);
     const idCustoLicenca = getCellValue(xlsx.utils.encode_col(columnNames['ID CUSTO LICENÇA']), row, worksheet);
-    const licencas = getCellValue(xlsx.utils.encode_col(columnNames['Licenças (extraído do Office365)']), row, worksheet);
-    const dataHoraCriacao = getCellValue(xlsx.utils.encode_col(columnNames['Data/hora de criação']), row, worksheet);
+    const dataHoraCriacaoValue = getCellValue(xlsx.utils.encode_col(columnNames['Data/hora de criação']), row, worksheet);
+    const dataHoraCriacao = isNaN(Date.parse(dataHoraCriacaoValue)) ? null : new Date(dataHoraCriacaoValue);
 
+    // Salvar os dados no banco de dados
     await Valor.create({
       distribuidora,
       idDistribuidora,
       nomeExibicao,
+      idLicenca,
       idCustoLicenca,
-      licencas,
       dataHoraCriacao
     });
   }
@@ -58,7 +63,6 @@ async function importData(worksheet) {
   console.log('Dados importados com sucesso!');
   process.exit(0);
 }
-
 
 module.exports = {
   importData
