87f95b63a58e325baf08809ac4cbc174feb890ce
Erison <erison7596e@gmail.com>
2023-07-13 12:17:48 -0300

    atualização, mas ainda permanece bug com a data

--- src/controllers/valorController.js
+++ src/controllers/valorController.js
@@ -1,4 +1,5 @@
 const xlsx = require('xlsx');
+const moment = require('moment');
 const db = require('../utils/database');
 const Valor = require('../models/valor');
 
@@ -18,7 +19,8 @@ async function importData(worksheet) {
     'Nome para exibição': -1,
     'ID LICENÇA': -1,
     'ID CUSTO LICENÇA': -1,
-    'Data/hora de criação': -1
+    'Data/hora de criação': -1,
+    'Licenças (extraído do Office365)': -1
   };
 
   for (let col = range.s.c; col <= range.e.c; col++) {
@@ -47,16 +49,43 @@ async function importData(worksheet) {
     const idLicenca = getCellValue(xlsx.utils.encode_col(columnNames['ID LICENÇA']), row, worksheet);
     const idCustoLicenca = getCellValue(xlsx.utils.encode_col(columnNames['ID CUSTO LICENÇA']), row, worksheet);
     const dataHoraCriacaoValue = getCellValue(xlsx.utils.encode_col(columnNames['Data/hora de criação']), row, worksheet);
-    const dataHoraCriacao = isNaN(Date.parse(dataHoraCriacaoValue)) ? null : new Date(dataHoraCriacaoValue);
+    console.log('Data/hora de criação (antes da formatação):', dataHoraCriacaoValue);
 
-    // Salvar os dados no banco de dados
+    let dataHoraCriacao;
+
+    if (dataHoraCriacaoValue !== undefined && dataHoraCriacaoValue !== 'Data/hora de criação') {
+      if (typeof dataHoraCriacaoValue === 'string') {
+        const dataHoraCriacaoFormatted = moment(dataHoraCriacaoValue, 'DD/MM/YYYY HH:mm').format('YYYY-MM-DD HH:mm:ss');
+        if (dataHoraCriacaoFormatted !== 'Invalid date') {
+          dataHoraCriacao = dataHoraCriacaoFormatted;
+        } else {
+          console.log('Formato de data/hora inválido:', dataHoraCriacaoValue);
+          continue;
+        }
+      } else if (typeof dataHoraCriacaoValue === 'number') {
+        dataHoraCriacao = moment('1900-01-01').add(dataHoraCriacaoValue, 'days').format('YYYY-MM-DD HH:mm:ss');
+      } else {
+        console.log('Tipo de valor inválido:', dataHoraCriacaoValue);
+        continue;
+      }
+    } else {
+      console.log('Valor de data/hora inválido:', dataHoraCriacaoValue);
+      continue;
+    }
+
+    console.log('Data/hora de criação (após a formatação):', dataHoraCriacao);
+
+    // Licenças (extraído do Office365)
+    const licencas = getCellValue(xlsx.utils.encode_col(columnNames['Licenças (extraído do Office365)']), row, worksheet);
+
     await Valor.create({
       distribuidora,
       idDistribuidora,
       nomeExibicao,
       idLicenca,
       idCustoLicenca,
-      dataHoraCriacao
+      dataHoraCriacao,
+      licencas
     });
   }
 
